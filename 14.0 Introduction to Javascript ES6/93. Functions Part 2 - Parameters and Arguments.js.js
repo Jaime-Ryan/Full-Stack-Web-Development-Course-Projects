29cb454ff443a431e4ac2234eb9670954b2af6a2
Jaime Ryan <jaimeryanlive@gmail.com>
2025-05-08 20:17:11 -0400

    93. Functions Part 2 - Parameters and Arguments.js 94. Life in Weeks Coding Exercise.js 95. Functions Part 3 - Outputs & Return Values.js 99. Random Number Generation in Javascript - Building a Love Calculator.js

---
+++ 14.0 Introduction to Javascript ES6/93. Functions Part 2 - Parameters and Arguments.js.js
@@ -0,0 +1,29 @@
+function getMilk(money) {
+    var bottles = Math.floor(money / 1.5);
+    var cost = bottles * 1.5;
+
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("buy " + bottles + " bottles of milk");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+
+    return money - cost;
+}
+
+var change = getMilk(5000);
+console.log("You have $" + change.toFixed(2) + " left.");
---
+++ 15.0 Intermediate Javascript/99. Random Number Generation in Javascript - Building a Love Cal. Random Number Generation in Javascript - Building a Love Calculator.js
@@ -0,0 +1,13 @@
+/* var n = Math.random();
+n = n * 6; 
+n = Math.floor(n) + 1;
+console.log(n);
+*/
+
+var yourName = prompt("What is your name?");
+var crushName = prompt("What is the name of your crush?")
+
+var loveChance = Math.random() * 100;
+loveChance = Math.floor(loveScore) +1;
+
+alert("The chance of love between " + yourName + " and " + crushName + " is " + loveChance + "%");
