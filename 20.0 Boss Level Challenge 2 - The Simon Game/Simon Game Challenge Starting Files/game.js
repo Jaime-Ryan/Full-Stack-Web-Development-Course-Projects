71970c2793213a4a9ba8e6238f54442c403855d7
Jaime Ryan <jaimeryanlive@gmail.com>
2025-05-17 07:49:12 -0400

    23.0 Node.js/2.1 Using Node 23.0 Node.js/2.2 Native Modules

--- 20.0 Boss Level Challenge 2 - The Simon Game/Simon Game Challenge Starting Files/game.js
+++ 20.0 Boss Level Challenge 2 - The Simon Game/Simon Game Challenge Starting Files/game.js
@@ -4,8 +4,30 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 // 5. Empty array to store the game pattern
 var gamePattern = [];
 
+// 3. Create an empty array to store the user's pattern
+var userClickedPattern = [];
+
+// 2. Create a new variable called level and start at level 0
+var level = 0;
+// Variable to track if the game has started
+var started = false;
+
+// 1. Detect keypress to start the game
+$(document).keydown(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level); // 3. Update h1 to say Level 0
+    nextSequence();
+    started = true;
+  }
+});
+
 // 1. Create the nextSequence function
 function nextSequence() {
+  // 4. Increase the level by 1 every time nextSequence() is called
+  level++;
+  // 5. Update the h1 with the new level
+  $("#level-title").text("Level " + level);
+
   // 2. Generate a random number between 0 and 3
   var randomNumber = Math.floor(Math.random() * 4);
   // 4. Select a random colour from buttonColours
@@ -19,9 +41,65 @@ function nextSequence() {
     .fadeOut(100).fadeIn(100);
 
   // 3. Play the sound for the button colour
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-// Call nextSequence to test
-nextSequence();
+function animatePress(currentColour) {
+  // 3. Add the 'pressed' class to the button
+  $("#" + currentColour).addClass("pressed");
+  // 4. Remove the 'pressed' class after 100 milliseconds
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+// 1. Create a new function called checkAnswer()
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+      userClickedPattern = [];
+    }
+  } else {
+    // Play the wrong sound
+    playSound("wrong");
+    // Add the 'game-over' class to the body
+    $("body").addClass("game-over");
+    // Remove the 'game-over' class after 200ms
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    // Change the h1 title
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    // Reset game state
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+  userClickedPattern = [];
+}
+
+// jQuery to detect when any button is clicked
+$(".btn").click(function() {
+  // 2. Store the id of the button that got clicked
+  var userChosenColour = $(this).attr("id");
+  // 4. Add the user's chosen colour to the userClickedPattern array
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour); // Play sound on button click
+  animatePress(userChosenColour); // Animate button press
+  // 2. Call checkAnswer with the index of the last answer
+  checkAnswer(userClickedPattern.length - 1);
+  // Log to verify
+  // console.log(userClickedPattern);
+});
